dd004248b2df927ce68e9cb5c1c61e0cdf6ddcc1
Cory DeGuzman <cory.deguzman@thedigitalohana.com>
2025-03-28 17:15:09 -0700

    VAUL-16 Setup for all subtasks

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Vaultly API')
+    .setDescription('API Documentation for Vaultly')
+    .setVersion('1.0')
+    .addTag('vaultly')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
